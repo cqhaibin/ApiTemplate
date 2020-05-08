9b36f50ebe6373ded548f7f2bc2006638ce830b1
sam Dragon <kxhaibin@163.com>
2020-05-08 09:43:16 +0800

    add signalr test
    Signed-off-by: sam Dragon <kxhaibin@163.com>

---
+++ test/ConsoleAppSignalrRClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace ConsoleAppSignalrRClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hubConnection = new HubConnection("http://127.0.0.1:9909/wsc");
+            IHubProxy hubProxy = hubConnection.CreateHubProxy("TestHub");
+            hubProxy.On("sendMessage", (mgs) =>
+            {
+                Console.WriteLine(mgs);
+            });
+            hubConnection.Start().Wait();
+            Console.ReadKey();
+        }
+    }
+}
